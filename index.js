1204adad5a34f37795f811007e5ecbad86b91e85
jesbell <joselyngonzalezma@gmail.com>
2024-04-09 17:43:00 -0400

    Agregando detalles, titulo, y ordenando el código

--- index.js
+++ index.js
@@ -2,31 +2,43 @@ import chalk from "chalk";
 import { v4 as uuidv4 } from "uuid";
 import moment from "moment";
 import _ from "lodash";
-import axios from "axios";
+import axios from "axios"; 
+import express from "express";
+
+const app = express();
 
 const cantPacientes = 10;
 
-axios.get(`https://randomuser.me/api/?results=${cantPacientes}`).then((data) => {
-    const pacientes = data.data.results.map(paciente => ({
-        ...paciente,
-        uuid: uuidv4().slice(0,6),
-        timestamp: moment().format('MMMM Do YYYY, h:mm:ss a')
-    }));
-    // agrupamos por gender
-    const grupoGenero = _.groupBy(pacientes, 'gender');
 
-    // iteramos por genero
+// Función para imprimir en consola, con letra azul y fondo blanco
+const verenConsola = (grupoGenero) => {
+    console.log(chalk.blue.bold.bgWhite("Lista de pacientes"))
     _.forEach(grupoGenero, (usuarios, genero) => {
-        if(genero == 'male'){
-            var gen = "Masculino";
-        }
-        else{
-            var gen = "Femenino";
-        }
-        console.log(`Usuarios de género ${gen}:`);
+        console.log(`${genero == 'male' ? 'Masculino' : 'Femenino'}:`);
         // Iterar sobre los usuarios de cada grupo
         _.forEach(usuarios, (usuario, i) => {
             console.log(chalk.blue.bgWhite(`${i + 1}. Nombre: ${usuario.name.first} - Apellido: ${usuario.name.last} - Género: ${usuario.gender} - uuid: ${usuario.uuid} - Timestamp: ${usuario.timestamp}`));
         });
     });
+};
+
+app.get("/", (req, res) => {
+    
+    axios.get(`https://randomuser.me/api/?results=${cantPacientes}`).then((data) => {
+        const pacientes = data.data.results.map(paciente => ({
+                    ...paciente,
+            uuid: uuidv4().slice(0,6),
+            timestamp: moment().format('MMMM Do YYYY, h:mm:ss a')
+        }));
+        // agrupamos por genero
+        const grupoGenero = _.groupBy(pacientes, 'gender');
+
+        verenConsola(grupoGenero);
+
+        res.json(grupoGenero);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
 });
